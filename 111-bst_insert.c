1421a6d90bfbea252c554b97e886c5b7ec3f7974
Sarolus <cactussius.primus@gmail.com>
2021-07-21 23:49:44 +0200

    Add correct task 25 prototype

---
+++ 111-bst_insert.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * 
+ * 
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	if (*tree == NULL)
+		return (binary_tree_node(NULL, value));
+	
+	if (value < (*tree)->n)
+		(*tree)->left = bst_insert(&(*tree)->left, value);
+	else if (value > (*tree)->n)
+		(*tree)->right = bst_insert(&(*tree)->right, value);
+
+	return (*tree);
+}
